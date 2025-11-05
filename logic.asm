9afcce3fe6996c605e28c6973e11622128282453
Nicholas Shenk <nickshenk68@gmail.com>
2025-11-05 17:36:22 -0500

    Set up structures and main function

--- logic.asm
+++ logic.asm
@@ -1,1 +1,17 @@
 INCLUDE ui.INC
+INCLUDE IRVINE32.INC
+.data
+card STRUCT
+ value BYTE ?
+ suit BYTE ?
+card ENDS
+Player STRUCT
+ cards WORD 4 DUP(0)
+Player ENDS
+.code
+main PROC
+    call ExitProcess
+  quit:
+    exit
+main ENDP
+END main
