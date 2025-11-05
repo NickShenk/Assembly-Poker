a57bc10eb735c9ae6f00292b31d414aed96101bb
NickShenk <nickshenk68@gmail.com>
2025-11-05 18:09:42 -0500

    Set up pseudo code for starting game

--- logic.asm
+++ logic.asm
@@ -2,14 +2,28 @@ INCLUDE ui.INC
 INCLUDE IRVINE32.INC
 .data
 card STRUCT
- value BYTE ?
- suit BYTE ?
+ value BYTE
+ suit BYTE
 card ENDS
+
+cards card 52 DUP(<>) ; Found online how to initialize a struct array https://stackoverflow.com/questions/75139036/array-of-custom-structs-masm-windows-api
+
 Player STRUCT
  cards WORD 4 DUP(0)
 Player ENDS
 .code
 main PROC
+    ; initialize cards
+    mov eax, 52
+    initialize:
+    dec eax
+
+    mov cards[eax].value, eax MOD 14
+    cards[eax].suit, eax / 4
+    cmp ax, eax
+    jnz initialize
+    
+
     call ExitProcess
   quit:
     exit
