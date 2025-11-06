2dc058da6e6d23c2c67599fe9c2860453ff70c89
NickShenk <nickshenk68@gmail.com>
2025-11-05 19:43:19 -0500

    Finished shuffle algorithm

--- logic.asm
+++ logic.asm
@@ -7,6 +7,8 @@ card STRUCT
 card ENDS
 
 cards card 52 DUP(<>) ; Found online how to initialize a struct array https://stackoverflow.com/questions/75139036/array-of-custom-structs-masm-windows-api
+random WORD ?
+
 
 Player STRUCT
  first_card BYTE 0 ; index of card in deck
@@ -36,12 +38,41 @@ main PROC
     modloop:
     sub eax, 13
     cmp eax, 13
-    jg modloop
+    jge modloop
     mov cards[ecx * TYPE card].value, al ; eax mod 13, al is right most byte
     
     cmp ecx,0 
     jnz initialize
     ; end of intialization
+    start:
+    ; shuffle deck
+    ;randomize learned using this https://stackoverflow.com/questions/10963554/generate-random-number-in-a-range-in-assembly
+    call randomize
+    mov ecx, 52
+    
+    shuffle:
+    dec ecx
+    ; Fisher Yates algorithm
+    mov eax, ecx
+    call randomrange
+    ; ecx = pos and eax = random pos to swap with
+    mov edx, eax
+    ; swap
+    ; swap values
+    mov al, cards[ecx * TYPE card].value
+    mov bl, cards[edx * TYPE card].value
+    mov cards[ecx * TYPE card].value, bl
+    mov cards[edx * TYPE card].value, al
+    ; swap suits
+    mov al, cards[ecx * TYPE card].suit
+    mov bl, cards[edx * TYPE card].suit
+    mov cards[ecx * TYPE card].suit, bl
+    mov cards[edx * TYPE card].suit, al
+    ; return to top of shuffle
+    
+    cmp ecx, 1
+    jg shuffle
+    ; end of shuffle
     
     ; fill out each player
 
