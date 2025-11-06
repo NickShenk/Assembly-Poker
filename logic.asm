092b114f157868dfd424b98351686b7d1346596d
ChristianPrieto243 <cprieto2023@my.fit.edu>
2025-11-06 09:41:20 -0500

    feat: add FindWinner(), showdown, replay loop, game-over

--- logic.asm
+++ logic.asm
@@ -1,10 +1,11 @@
-; COMMIT 6
-; Why: Add a tiny betting interaction so pot/chips change.
-; What: Human: C/R/F. AI randomly folds/checks/calls. Uses WORD money.
+;  7
+; Why: Determine winner by highest card; loop hands until broke.
+; What: Adds FindWinner + continue prompt + game over check.
 
 INCLUDE Irvine32.inc
 
 .data
+;
 deck BYTE 2,3,4,5,6,7,8,9,10,11,12,13,14
      BYTE 2,3,4,5,6,7,8,9,10,11,12,13,14
      BYTE 2,3,4,5,6,7,8,9,10,11,12,13,14
@@ -35,32 +36,14 @@ aiCallStr  BYTE "AI calls",0Dh,0Ah,0
 aiCheckStr BYTE "AI checks",0Dh,0Ah,0
 aiFoldStr  BYTE "AI folds",0Dh,0Ah,0
 
+winP1 BYTE 0Dh,0Ah,"*** YOU WIN $",0
+winP2 BYTE 0Dh,0Ah,"*** AI WINS $",0
+winEnd BYTE " ***",0Dh,0Ah,0
+contMsg BYTE 0Dh,0Ah,"Press any key for next hand...",0
+overMsg BYTE 0Dh,0Ah,"GAME OVER - Someone is broke!",0Dh,0Ah,0
 raiseAmt WORD 10
 
 .code
-
-; SimpleShuffle
-SimpleShuffle PROC
-    push eax ebx ecx
-    mov ecx, 30
-@@L:
-    mov eax,52
-    call RandomRange
-    mov ebx,eax
-    mov eax,52
-    call RandomRange
-    push ecx
-    mov cl, deck[ebx]
-    mov ch, deck[eax]
-    mov deck[ebx], ch
-    mov deck[eax], cl
-    pop ecx
-    loop @@L
-    pop ecx ebx eax
-    ret
-SimpleShuffle ENDP
-
-; PrintCard
 PrintCard PROC
     push eax
     push edx
@@ -159,97 +142,84 @@ BettingRound PROC
     push ecx
     push edx
 
-    ; Human
-    mov edx, OFFSET yourTurn
-    call WriteString
-    call ReadChar
-    call WriteChar
-    call Crlf
 
-    cmp al,'F'
-    je @p1fold
-    cmp al,'f'
-    je @p1fold
-    cmp al,'R'
-    je @p1raise
-    cmp al,'r'
-    je @p1raise
+; Winner by highest of the two cards each
+FindWinner PROC
+    push eax
+    push ebx
+    push ecx
+    push edx
 
-    ; default call/check
-    mov ax, currentBet
-    mov bx, p1bet
-    sub ax, bx
-    cmp ax,0
-    je @p1done
-    add p1bet, ax
-    sub p1chips, ax
-    add pot, ax
-    jmp @p1done
+    ; best P1 in CL
+    mov al, p1card1
+    mov bl, p1card2
+    cmp al, bl
+    jge @p1ok
+    mov al, bl
+@p1ok:
+    mov cl, al
 
-@p1raise:
-    mov ax, raiseAmt
-    add currentBet, ax
-    mov ax, currentBet
-    mov bx, p1bet
-    sub ax, bx
-    add p1bet, ax
-    sub p1chips, ax
-    add pot, ax
-    jmp @p1done
+    ; best P2 in DL
+    mov al, p2card1
+    mov bl, p2card2
+    cmp al, bl
+    jge @p2ok
+    mov al, bl
+@p2ok:
+    mov dl, al
 
-@p1fold:
+    cmp cl, dl
+    jg  @p1wins
+    jl  @p2wins
+
+    ; tie -> split pot
     mov ax, pot
+    shr ax, 1
+    add p1chips, ax
     add p2chips, ax
-    mov pot, 0
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    ret
-
-@p1done:
-    ; AI decision
-    mov eax,100
-    call RandomRange
-    cmp eax,20
-    jl @aifold
-
-    ; call/check
-    mov ax, currentBet
-    mov bx, p2bet
-    sub ax, bx
-    cmp ax,0
-    je @aicheck
-    add p2bet, ax
-    sub p2chips, ax
-    add pot, ax
-    mov edx, OFFSET aiCallStr
-    call WriteString
     jmp @done
 
-@aicheck:
-    mov edx, OFFSET aiCheckStr
-    call WriteString
-    jmp @done
-
-@aifold:
+@p1wins:
     mov ax, pot
     add p1chips, ax
-    mov pot, 0
-    mov edx, OFFSET aiFoldStr
+    mov edx, OFFSET winP1
+    call WriteString
+    movzx eax, pot
+    call WriteDec
+    mov edx, OFFSET winEnd
+    call WriteString
+    jmp @done
+
+@p2wins:
+    mov ax, pot
+    add p2chips, ax
+    mov edx, OFFSET winP2
+    call WriteString
+    movzx eax, pot
+    call WriteDec
+    mov edx, OFFSET winEnd
     call WriteString
 
 @done:
+    mov pot, 0
     pop edx
     pop ecx
     pop ebx
     pop eax
     ret
-BettingRound ENDP
+FindWinner ENDP
 
 main PROC
+NewHand:
+    ; reset hand state
+    mov pot, 0
+    mov currentBet, 0
+    mov p1bet, 0
+    mov p2bet, 0
+
     call Randomize
     call SimpleShuffle
+
     mov al, deck[0]
     mov p1card1, al
     mov al, deck[1]
@@ -263,6 +233,24 @@ main PROC
     call BettingRound
     call ShowTable
 
+    cmp pot, 0
+    je @skipShowdown
+    call FindWinner
+
+@skipShowdown:
+    mov edx, OFFSET contMsg
+    call WriteString
+    call ReadChar
+
+    cmp p1chips, 0
+    jle @gameover
+    cmp p2chips, 0
+    jle @gameover
+    jmp NewHand
+
+@gameover:
+    mov edx, OFFSET overMsg
+    call WriteString
     INVOKE ExitProcess, 0
 main ENDP
 END main
