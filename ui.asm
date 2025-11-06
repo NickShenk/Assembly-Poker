d7fbf8671c05345b90638c9428da39565655403c
ChristianPrieto243 <cprieto2023@my.fit.edu>
2025-11-05 19:21:54 -0500

    welcome

--- ui.asm
+++ ui.asm
@@ -1,2 +1,15 @@
+; ui.asm - Poker UI routines - Author: Christian Prieto
+INCLUDE Irvine32.inc
 
-;test 
+.data
+welcomeMsg BYTE "Welcome to MASM Poker!", 0
+
+.code
+main PROC
+    mov edx, OFFSET welcomeMsg   ; Set pointer to string for display
+    call WriteString             ; prints string at EDX
+    call Crlf                   ; Newline for output clarity
+    ; UI rendering and menu logic will go below heree
+    exit
+main ENDP
+END main
